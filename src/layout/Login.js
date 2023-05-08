dd3c141823f52b871cc2f0d1a74ab5097739c66a
Eshrat <eshratjahan701@gmail.com>
2023-05-08 17:26:20 +0600

    form created

--- src/layout/Login.js
+++ src/layout/Login.js
@@ -1,11 +1,58 @@
-import React from 'react';
+import React from "react";
+import { useForm } from "react-hook-form";
 
 const Login = () => {
-    return (
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => console.log(data);
+
+  console.log(watch("example"));
+  return (
+    <div className="flex items-center justify-center flex-col h-screen ">
+      <form onSubmit={handleSubmit(onSubmit)}>
+        {/* register your input into the hook by invoking the "register" function */}
         <div>
-            <h1>This is log in page ...</h1>
+          <div className="form-control w-full ">
+            <label className="label">
+              <span className="label-text">Email</span>
+            </label>
+            <input
+              type="email"
+              placeholder="Email here"
+              className="input input-bordered w-full "
+              {...register("email", { required: true })}
+            />
+            <label className="label">
+              <span className="label-text-alt">Bottom Left label</span>
+            </label>
+          </div>
         </div>
-    );
+        <div>
+          <div className="form-control w-full max-w-xs">
+            <label className="label">
+              <span className="label-text">Password</span>
+            </label>
+            <input
+              required
+              type="email"
+              placeholder="Password here"
+              className="input input-bordered w-full max-w-xs"
+              {...register("password", { required: true })}
+            />
+            <label className="label">
+              <span className="label-text-alt">Bottom Left label</span>
+            </label>
+          </div>
+        </div>
+
+        <input type="submit" />
+      </form>
+    </div>
+  );
 };
 
 export default Login;
